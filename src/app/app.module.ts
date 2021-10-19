6f87a87edd6ed685318c549e7783ef994885418b
ndshen <andyshen1222@gmail.com>
2021-10-19 10:35:02 +0800

    create post-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostCardComponent } from './post-card/post-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
